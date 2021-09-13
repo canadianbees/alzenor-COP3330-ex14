0b00de213fa11f25d214428edd665e3eb8bd3773
Celina Alzenor <you@example.com>
2021-09-13 15:18:07 -0400

    updated code to fit the constraints

--- src/main/java/org/example/Store.java
+++ src/main/java/org/example/Store.java
@@ -25,20 +25,22 @@ public class Store
 
     public void TaxCalc()
     {
+        String finalOut;
         if(this.state.matches("WI") || this.state.matches("wi") || this.state.matches("Wi") || this.state.matches("wI"))
         {
-            DecimalFormat round = new DecimalFormat("###.##");
+            DecimalFormat round = new DecimalFormat("##0.00");
             float tax = (float) (5.5/100);
             float total = (this.orderAmt*tax)+this.orderAmt;
 
-            System.out.println("The subtotal is $"+this.orderAmt+".");
-            System.out.println("The tax is $"+(round.format(this.orderAmt*tax))+".");
-            System.out.println("The total is $"+round.format(total)+".");
-
-            return;
+            finalOut = "The subtotal is $"+this.orderAmt+".\nThe tax is $"+(round.format(this.orderAmt*tax))+".\nThe total is $"+round.format(total)+".";
         }
 
-        System.out.println("The total is $"+this.orderAmt+".");
+        else
+        {
+            finalOut = "The total is $"+this.orderAmt+".";
+        }
+
+        System.out.print(finalOut);
     }
 
 
